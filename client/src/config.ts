fec0b20b9f2f3d9de0bedc9fc37348058b467434
lam.bui <lam.bui@cloudgo.vn>
2026-03-05 22:16:57 +0700

    Add Zod validation schemas and HTTP utility functions

---
+++ client/src/config.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+const configSchema = z.object({
+  NEXT_PUBLIC_API_ENDPOINT: z.string(),
+  NEXT_PUBLIC_CLIENT_URL: z.string()
+})
+
+const config = configSchema.safeParse({
+	NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+})
+
+if (!config.success) {
+  console.error(config.error.issues)
+  throw new Error('Các giá trị khai báo trong file .env không hợp lệ')
+}
+
+const envConfig = config.data
+
+export default envConfig
