35b6c2fe8c1a5294dfbba5297fdf6c373c98b666
lam.bui <lam.bui@cloudgo.vn>
2026-03-15 14:43:19 +0700

    Code route handler logout

--- client/src/apiRequests/auth.ts
+++ client/src/apiRequests/auth.ts
@@ -1,9 +1,15 @@
 import http from "@/lib/http";
-import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema";
+import { LoginBodyType, LoginResType, LogoutBodyType } from "@/schemaValidations/auth.schema";
 
 const authApiRequest = {
 	sLogin: (body: LoginBodyType) => http.post<LoginResType>('/auth/login', body), // Server-side login
 	login: (body: LoginBodyType) => http.post<LoginResType>('/api/auth/login', body, { baseUrl: '' }), // Client-side login
+	sLogout: (body: LogoutBodyType & { accessToken: string }) => http.post('/auth/logout', { refreshToken: body.refreshToken }, {
+		headers: {
+			Authorization: `Bearer ${body.accessToken}`
+		}
+	}),
+	logout: () => http.post('/api/auth/logout', null, { baseUrl: '' }),
 }
 
 export default authApiRequest
