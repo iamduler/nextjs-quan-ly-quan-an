36b5fff4096f6b16ec09b3bc180dcfbf8a5d2ed9
lam.bui <lam.bui@cloudgo.vn>
2026-04-08 17:01:58 +0700

    Khai báo API cho accounts

--- client/src/queries/useAccount.tsx
+++ client/src/queries/useAccount.tsx
@@ -1,5 +1,6 @@
 import accountApiRequest from "@/apiRequests/account"
-import { useMutation, useQuery } from "@tanstack/react-query"
+import { UpdateEmployeeAccountBodyType } from "@/schemaValidations/account.schema"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 
 export const useAccountMeQuery = () => {
 	return useQuery({
@@ -17,5 +18,49 @@ export const useUpdateMeMutation = () => {
 export const useChangePasswordMutation = () => {
 	return useMutation({
 		mutationFn: accountApiRequest.changePassword,
+	})
+}
+
+export const useGetAccountListQuery = () => {
+	return useQuery({
+		queryKey: ['accounts'],
+		queryFn: accountApiRequest.list,
+	})
+}
+
+export const useGetAccountQuery = ({ id }: { id: number }) => {
+	return useQuery({
+		queryKey: ['account', id],
+		queryFn: () => accountApiRequest.get(id),
+	})
+}
+
+export const useAddAccountMutation = () => {
+	const queryClient = useQueryClient()
+	return useMutation({
+		mutationFn: accountApiRequest.add,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['accounts'] })
+		},
+	})
+}
+
+export const useUpdateAccountMutation = () => {
+	const queryClient = useQueryClient()
+	return useMutation({
+		mutationFn: ({ id, ...body }: UpdateEmployeeAccountBodyType & { id: number }) => accountApiRequest.update(id, body),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['accounts'] })
+		},
+	})
+}
+
+export const useDeleteAccountMutation = () => {
+	const queryClient = useQueryClient()
+	return useMutation({
+		mutationFn: (id: number) => accountApiRequest.delete(id),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['accounts'] })
+		},
 	})
 }
